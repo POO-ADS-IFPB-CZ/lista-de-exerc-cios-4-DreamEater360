1877c3bfee6eda6bb3d709e3386fc6f35c9e7e49
Francisco Erlyson Pamplona <77758434+DreamEater360@users.noreply.github.com>
2025-05-19 07:24:40 -0300

    Update Main.java

--- questao4/src/view/Main.java
+++ questao4/src/view/Main.java
@@ -6,9 +6,9 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        cartaoCredito cartao = new cartaoCredito(1.2);
-        pix chave = new pix(LocalDate.now(), true);
-        boleto internet = new boleto(1.3, true);
+        pagamento cartao = new cartaoCredito(1.2);
+        pagamento chave = new pix(LocalDate.now(), true);
+        pagamento internet = new boleto(1.3, true);
 
         cartao.processar(10);
         chave.processar(10);
